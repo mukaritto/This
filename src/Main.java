3139cc7b4be548faedc7bcfb45e7d626d1f35e09
mukaritto <mukarit2006@gmail.com>
2024-09-09 03:11:05 +0500

    Just

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Doctor doctor1 = new Doctor();
+        doctor1.Ishxona();
+
+        Xirurg xirurg1 = new Xirurg();
+        xirurg1.Ishxona();
+        xirurg1.davolashJoyi();
+
+        Lor lor1 = new Lor();
+        lor1.Ishxona();
+        lor1.davolashJoyi();
+    }
+}
